6b0ad36dcc5fb1f91ac6fed248791eb754cc072d
Anas Rizvi <anas01riz@gmail.com>
2019-11-18 12:18:31 +0530

    table

---
+++ CoreJavaBasics/src/table/Table.java
@@ -0,0 +1,25 @@
+package table;
+
+import java.util.Scanner;
+
+public class Table {
+
+	public static void main(String[] args) {
+		
+			int n, i;
+			Scanner st= new Scanner(System.in);
+			System.out.println("Table");
+			System.out.println("Enter the table no : ");
+			 n = st.nextInt();
+			 for (i=1;i<=10;i++) {
+				 System.out.println(n+"*"+ i+"="+n*i);
+			 }
+			
+
+		}
+
+// TODO Auto-generated method stub
+
+	}
+
+
